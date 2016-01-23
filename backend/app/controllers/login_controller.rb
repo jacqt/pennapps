c8b5315275acb0f77ba8bd9f87f586f1e9630d45
David Hsu <dvdhsu@gmail.com>
2016-01-23 10:00:55 -0500

    Login with password instead of auth_token.

--- backend/app/controllers/login_controller.rb
+++ backend/app/controllers/login_controller.rb
@@ -1,7 +1,7 @@
 class LoginController < ApplicationController
   def login
-    @society = Society.where(email: params[:email], auth_token: params[:auth_token]).first
-    if @society.nil?
+    @society = Society.where(email: params[:email]).first
+    if @society.nil? or not @society.authenticate(params[:password])
       render json: { status: "failure" }, status: 404 and return
     else
       render json: { status: "success", society: @society }
