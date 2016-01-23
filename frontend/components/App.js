5446a45f9517e154e2e7af515e18e04c9292b800
Taimur <pain@live.co.za>
2016-01-23 11:56:08 -0500

    dasda

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -2,16 +2,41 @@ import React, { Component, PropTypes } from 'react'
 import { routeActions } from 'redux-simple-router'
 import { connect } from 'react-redux'
 
-import LandingPage from './LandingPage'
-
+import Header from './Header'
+import DashboardItem from './DashboardItem'
+import DashboardActions from './DashboardActions'
 
 class App extends Component {
   render() {
     const { dispatch } = this.props
     return (
-      <p>
-      <button onClick={() => this.props.dispatch(routeActions.push('/hi'))}>landing</button>
-      </p>
+      <div>
+        <Header></Header>
+        <div className='ui container'>
+          <div className='ui two column stackable grid bottom aligned'>
+            <div className='twelve wide column left aligned'>
+              <div className='dashboard-name'>Oxford University Islamic Society</div>
+            </div>
+            <div className='four wide column right aligned'>
+              <h2>Balance</h2>
+              <div className='dashboard-balance'>£315.00</div>
+            </div>
+          </div>
+          <hr/>
+          <div className='ui two column stackable grid'>
+            <div className='twelve wide column left aligned'>
+              <h1>Your Items</h1>
+              <DashboardItem name='Teddy Hall Formal' price='13.00' remaining='12' capacity='25'/>
+              <DashboardItem name='Teddy Hall Formal' price='13.00' remaining='12' capacity='25'/>
+              <DashboardItem name='Teddy Hall Formal' price='13.00' remaining='12' capacity='25'/>  
+            </div>
+            <div className='four wide column right aligned'>
+              <h1>Actions</h1>
+              <DashboardActions/>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
--- frontend/components/LandingPage.js
+++ frontend/components/LandingPage.js
@@ -3,9 +3,31 @@ import React, { Component, PropTypes } from 'react'
 class LandingPage extends Component {
   render() {
     return (
-      <p>
-        LandingPage
-      </p>
+      <div className='masthead ui'>
+      	<div className='ui large top menu'>
+      		<div className='ui container'>
+      			<div className='logo item'>PaySpace</div>
+      			<div className='right item'>
+      				<a className='item'>Login</a>
+      			</div>
+      		</div>
+      	</div>
+      	<div className='ui text container centered'>
+      		<h1>Start accepting online payments in 60 seconds.</h1>
+      		<form className='ui form signup'>
+      			<div className='field'>
+      				<input type="text" name="name" placeholder="Society name"/>
+      			</div>
+      			<div className='field'>
+      				<input type="text" name="email" placeholder="Email address"/>
+      			</div>
+      			<div className='field'>
+      				<input type="password" name="password" placeholder="Password"/>
+      			</div>
+      			<button className="ui button" type="submit">Get Started</button>
+      		</form>
+      	</div>
+      </div>
     )
   }
 }
