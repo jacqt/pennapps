346744f732ae64f74c035273378babf60588579e
Lukas Koebis <lukaskoebis@gmail.com>
2016-01-24 01:06:41 +0000

    ES6ified objectToFormData

--- frontend/lib/ajax.js
+++ frontend/lib/ajax.js
@@ -1,40 +1,29 @@
 import { API_BASE } from './urls'
 
-//const fetch = require('whatwg-fetch')
+//const fetch = require('whatwg-fetch') // TODO?
 const url = require('url')
 
-var objectToFormData = function(obj, form, namespace) {
+function objectToFormData(obj, form, namespace) {
+  const fd = form || new FormData()
+  let formKey
 
-  var fd = form || new FormData();
-  var formKey;
-
-  for(var property in obj) {
+  for(const property in obj) {
     if(obj.hasOwnProperty(property)) {
-
       if(namespace) {
-        formKey = namespace + '[' + property + ']';
+        formKey = namespace + '[' + property + ']'
       } else {
-        formKey = property;
+        formKey = property
       }
 
-      // if the property is an object, but not a File,
-      // use recursivity.
       if(typeof obj[property] === 'object' && !(obj[property] instanceof File)) {
-
-        objectToFormData(obj[property], fd, property);
-
+        objectToFormData(obj[property], fd, property)
       } else {
-
-        // if it's a string or a File object
-        fd.append(formKey, obj[property]);
+        fd.append(formKey, obj[property])
       }
-
     }
   }
-
   return fd;
-
-};
+}
 
 export function signup(email, password, passwordConfirmation, name, nickname) {
   const f = objectToFormData({
