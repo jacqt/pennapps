e53dad9bdcc6dfa216d9ec54467e2fae83f18ba1
David Hsu <dvdhsu@gmail.com>
2016-01-23 23:55:39 -0500

    Add payment route.

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   scope 'v1' do
     get 'client_token' => 'payments#client_token'
+    post 'payments' => 'payments#purchase'
     get 'society' => 'societies#show'
     post 'society' => 'societies#update'
     post 'items' => 'items#create'
