2f6aa5aee993fd79abce72fccfb89eb9e7d343b3
Lukas Koebis <lukaskoebis@gmail.com>
2016-02-02 00:42:10 +0000

    edit cancel button

--- frontend/components/EditItem.js
+++ frontend/components/EditItem.js
@@ -10,6 +10,7 @@ class EditItem extends Component {
     	  <input type='text' name='name' placeholder='Item name' defaultValue={oldItem ? oldItem.name : null}/>
         <input type='number' name='price' placeholder='Item price' defaultValue={oldItem ? oldItem.price : null}/>
         <input type='number' name='capacity' placeholder='Capacity' defaultValue={oldItem ? oldItem.capacity : null}/>
+        <button onClick={() => this.props.abort()}>Cancel</button>
         <button onClick={() => this.props.action($('input[name="name"]').val(),$('input[name="price"]').val(),$('input[name="capacity"]').val())}>Edit Item</button>
   		  </div>
       </div>
