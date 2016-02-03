b2b33a8892ab0fd23740cf0041c39ba9bed3a040
Lukas Koebis <lukaskoebis@gmail.com>
2016-02-03 16:13:07 +0000

    updated deploy script

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh deployer@oatpay.com  "cd /srv/pennapps/frontend && git pull && npm run deploy && ./purge_cloudfare.sh"
+ssh deployer@129.67.36.198  "cd /srv/pennapps/frontend && git pull && npm install && npm run deploy && ./purge_cloudfare.sh"
