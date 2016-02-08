0aa1105413c138299d9e8bdb208e0ca81aec1794
Taimur <pain@live.co.za>
2016-02-08 15:23:24 +0000

    dasd

--- frontend/components/public/LandingPage.js
+++ frontend/components/public/LandingPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router'
 
 import * as Actions from '../../actions/userActions'
 
@@ -85,7 +86,7 @@ class LandingPage extends Component {
       			</button>
       		</div>
           <div className='convinced centere'>Not convinced?</div>
-          <button className='demo centered'>Check out a demo! TODO(Taimur): link to existing payment page (open in new tab)</button>
+          <Link to='' target="_blank"><button className='demo centered'>Check out a demo!</button></Link>
       	</div>
       </div>
       <div className='ui row row2'>
