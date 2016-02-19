a890b88bad6ea75a5900d937e01b3856f8e45894
David Hsu <dvdhsu@gmail.com>
2016-02-19 16:27:53 +0000

    Fix bug with validating a non-existent field in Item.

--- backend/app/models/item.rb
+++ backend/app/models/item.rb
@@ -6,7 +6,6 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :price_cents, presence: true
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 
   validates_numericality_of :capacity, only_integer: true, greater_than_or_equal_to: 0, message: "capacity must be integral and nonnegative"
   validates_numericality_of :price, greater_than_or_equal_to: 0, message: "capacity must be integral and nonnegative"
