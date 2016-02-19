a85a92d7b0cfc17138d844bdcf331bd77bf53557
David Hsu <dvdhsu@gmail.com>
2016-02-19 16:29:36 +0000

    Fix bug in validation of Item price.

--- backend/app/models/item.rb
+++ backend/app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
   validates :price_cents, presence: true
 
   validates_numericality_of :capacity, only_integer: true, greater_than_or_equal_to: 0, message: "capacity must be integral and nonnegative"
-  validates_numericality_of :price, greater_than_or_equal_to: 0, message: "capacity must be integral and nonnegative"
+  validates_numericality_of :price_cents, greater_than_or_equal_to: 0, message: "capacity must be integral and nonnegative"
 
   def payments_count
     self.payments.count
