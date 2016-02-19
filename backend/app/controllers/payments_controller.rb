681ab07f66717d98cda85f6b978076f2952fb08c
David Hsu <dvdhsu@gmail.com>
2016-02-19 15:53:39 +0000

    Remove Braintree and use Stripe.

--- backend/app/controllers/payments_controller.rb
+++ backend/app/controllers/payments_controller.rb
@@ -5,32 +5,37 @@ class PaymentsController < ApplicationController
 
   def purchase
     @item = Item.find(params[:id])
-    nonce = params[:payment_method_nonce]
 
     if @item.payments_count > @item.capacity
       render json: { errors: { "item": "is sold out" } }, status: 400 and return
     end
 
-    result = Braintree::Transaction.sale(
-      :amount => @item.price.format(symbol: false),
-      :payment_method_nonce => nonce
+    customer = Stripe::Customer.create(
+      :email => payment_params[:email],
+      :card  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @item.price.price_cents,
+      :description => "#{@item.name}, from society #{@item.society.name}",
+      :currency    => 'gbp'
     )
 
     @payment = @item.payments.build(payment_params)
 
-    if result.success?
-      @payment.save!
-      @item.society.balance += @item.price
-      @item.society.save!
-      render json: { data: { payment: @payment } }
-    else
-      render json: { errors: result.errors }, status: 400
-    end
+    @payment.save!
+    @item.society.balance += @item.price
+    @item.society.save!
+    render json: { data: { payment: @payment } }
+
+    rescue Stripe::CardError => e
+      render json: { errors: e.message }, status: 400
   end
 
   private
 
   def payment_params
-    params.require(:payment).permit(:name, :email)
+    params.require(:payment).permit(:email)
   end
 end
