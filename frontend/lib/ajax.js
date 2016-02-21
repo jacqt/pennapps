61655220c1f1413de9538460c46f115ad69a4df7
Taimur <pain@live.co.za>
2016-02-21 23:45:14 +0000

    dasd

--- frontend/lib/ajax.js
+++ frontend/lib/ajax.js
@@ -167,11 +167,8 @@ export function getClientToken() {
   return processResponse(fetch(requestUrl))
 }
 
-<<<<<<< HEAD
-export function pay(email, itemId, nonce) {
-=======
+
 export function pay(email, itemId, stripeToken) {
->>>>>>> d7e473f784842e3362ae385822815a0488ac9eb4
   const f = objectToFormData({
     payment: {
       email: email,
