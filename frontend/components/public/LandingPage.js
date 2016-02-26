bc2458296dc8c0df4c3992bd843ad1df3e917e12
=AliAbdaal <=>
2016-02-26 02:20:27 +0000

    changes to landing page

--- frontend/components/public/LandingPage.js
+++ frontend/components/public/LandingPage.js
@@ -41,7 +41,7 @@ class LandingPage extends Component {
 				<div className='errorbanner'>Something went wrong: {this.props.error.message}</div>
 			)
 		}
-		let heading = 'Start accepting online payments for your society.'
+		let heading = 'Hate bank transfers? So do we.'
 		if (this.props.referer === 'fb') {
 			heading = 'Start accepting online payments.' // TODO(Taimur)
 		}
@@ -69,7 +69,7 @@ class LandingPage extends Component {
       	</div>
       	<div className='ui text container centered'>
       		<h1>{heading}</h1>
-          <h2>Say goodbye to bank transfers, collecting cash, & spreadsheets.</h2>
+          <h2>Oatpay lets your society accept online card payments in around 30 seconds. </h2>
       		<div className={'ui form signup ' + (this.state.signupLoading ? 'loading' : '')}>
       			<div className='field'>
       				<input type="text" name="name" className='signupfield' placeholder="Society name" tabIndex='1'/>
@@ -85,13 +85,48 @@ class LandingPage extends Component {
       			</button>
       		</div>
           <div className='convinced centere'>Not convinced?</div>
-          <Link to='' target="_blank"><button className='demo centered'>Check out a demo!</button></Link>
+          <Link to='https://oatpay.com/CambridgeUniversityIslamicSociety' target="_blank"><button className='demo centered'>Check out a demo!</button></Link>
       	</div>
       </div>
+
       <div className='ui row row2'>
-      <h2>Some of our happy users:</h2>
-      <Link to='http://oxfordmicro.org/' target="_blank"><img src='img/omi-logo.png' className='society-logo omi'/></Link>
-      <Link to='http://ouisoc.org/' target="_blank"><img src='img/isoc-logo.png' className='society-logo isoc'/></Link>
+
+      </div>
+
+      <div className='ui row grid stackable life-easier'>
+        <div className="ui eight wide column green-gradient">
+          <div className='ui container'>
+            <h3>Oatpay makes life easier for societies</h3>
+            <p>Students are really bad at paying for things on time. This is annoying when your society’s selling event tickets/stash/anything. Why not make it easier for everyone by accepting debit cards? </p>
+            <p>It takes literally 30 seconds to setup an account and add your first item/event. Your society gets its own Oatpay URL which you can send around, and students can pay for the items with their debit cards. </p>
+          </div>
+        </div>
+        <div className="ui eight wide column red-gradient">
+          <div className='ui container'>
+            <h3>Oatpay makes life easier for students</h3>
+            <p>Bank transfers are the worst thing ever. Who has time to login to online banking, dig out the card-reading machine thing, setup a new payee, enter the verification codes, and then scream in frustration when you get an error code? No one.</p>
+            <p>Oatpay saves you from this pain. You just visit the payment page (hopefully linked from the Facebook event/email), enter your debit card details, and you’re done. </p>
+          </div>
+        </div>
+      </div>
+
+      <div className='ui row catch-fees'>
+        <div className='ui container centered'>
+          <h3>The only catch - card processing fees</h3>
+          <p>Okay, here comes the annoying bit. It costs a small amount (20p + 0.17%) to process card payments. So if you’re a society that wants to sell a formal ticket for £12, your members will pay £12.40 by card instead.</p>
+          <p>We’ve done some informal surveys and students have differing views on this. Some say “meh it’s 40p who cares”, while others are adamant that they would prefer to waste 5 minutes of their life doing a bank transfer than spend an excess 40p. </p>
+          <p>The solution to this dilemma is simple. Give your students the option! If they want, they can save time and effort by using Oatpay, and pay the card transaction fee. If they’d rather not pay the 40p, they’re very welcome to bank transfer or pay in cash (which is probably what they’re doing anyway).</p>
+          <p>There’s literally nothing to lose. Might as well give Oatpay a shot.</p>
+          <p>Oh, and by the way, we don’t charge anything extra. At all. We made this website as a side-project over a weekend, and don’t care about making a penny from it. If university students end up using and liking Oatpay, then great - we’ve created something useful. That’s good enough for us.</p>
+        </div>
+      </div>
+
+      <div className='ui row row-happy-users'>
+      <h2 className='ui text container centered'>Some of our happy users</h2>
+        <div className='ui container society-logos centered'>
+          <Link to='http://oxfordmicro.org/' target="_blank"><img src='img/omi-logo.png' className='society-logo omi'/></Link>
+          <Link to='http://ouisoc.org/' target="_blank"><img src='img/isoc-logo.png' className='society-logo isoc'/></Link>
+        </div>
       </div>
       </div>
     )
