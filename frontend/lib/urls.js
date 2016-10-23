57accc88b9f05ac30a9ab7c7aae19254d67fd0bc
Anthony Guo <anthony_guo@live.com>
2016-10-23 01:56:44 -0400

    Change urls

--- frontend/lib/urls.js
+++ frontend/lib/urls.js
@@ -1,8 +1,8 @@
 // const DOMAIN = 'https://oatpay.com'
 // const API_BASE = 'https://api.oatpay.com/v1'
 //
-const DOMAIN = 'https://oatpay.com'
-const API_BASE = 'http://localhost:3000/v1'
+const DOMAIN = 'https://getcashew.com/oatpay'
+const API_BASE = 'https://api.getcashew.com/v1'
 
 export default {
   DOMAIN,
