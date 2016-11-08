17f67eb09db58bb8c34820b71a691baae9c62512
Anthony Guo <anthony_guo@live.com>
2016-11-07 22:44:16 -0500

    Redirect to st hughs ball merchant site

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -34,7 +34,7 @@ class App extends Component {
       return <LandingPage referer={this.props.location.query.ref}/>
     } else {
       console.log("!!!");
-      window.location = "/asdf"
+      window.location = "/StHugh'sBallc"
     }
   }
 }
