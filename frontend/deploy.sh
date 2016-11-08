08199f70a791059a041787f4c2dd9df3694b71ff
Anthony Guo <anthony_guo@live.com>
2016-11-07 22:22:33 -0500

    Add deploy script

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,2 +1,37 @@
 #!/bin/bash
-ssh deployer@129.67.36.198  "cd /srv/pennapps/frontend && git pull && npm install && npm run deploy && ./purge_cloudfare.sh"
+
+# See https://sipb.mit.edu/doc/safe-shell/
+set -euf -o pipefail
+
+RED="\e[31m"
+GREEN="\e[32m"
+NORMAL="\e[0m"
+GREY="\e[37m"
+LIGHTBLUE="\e[94m"
+
+SSH_SERVER="deployer@cashew-staging"
+TAR_DEST="~/oatpay-tars"
+SITE_DIR="/srv/braintree_client/static_site"
+
+echo -e "Generating bundle.js ...$LIGHTBLUE"
+# npm run deploy
+echo -e "$GREEN Generated bundle.js $NORMAL"
+
+echo -e "Generating tar package...$LIGHTBLUE"
+# current commit
+commit="$(git log | head -n 1 | cut -d ' ' -f 2 | cut -c1-10)" || true
+tarball="cashew-oatpay.$commit.tgz"
+tar -cvf $tarball dist
+
+echo -e "$GREEN Generated tar ball $tarball $NORMAL"
+
+echo -e "Copying tar ball...$LIGHTBLUE"
+scp "./$tarball" $SSH_SERVER:$TAR_DEST
+echo -e "$GREEN Copied tar ball $NORMAL"
+
+echo -e "Deploying tar ball...$LIGHTBLUE"
+ssh $SSH_SERVER "tar -xvf $TAR_DEST/$tarball --directory $TAR_DEST"
+ssh $SSH_SERVER "rm -rf $SITE_DIR && mkdir $SITE_DIR && mv $TAR_DEST/dist/* $SITE_DIR"
+echo -e "$GREEN Deployed tar ball to $SITE_DIR $NORMAL"
+
+rm $tarball
